bcad74ff319301fd1c496dfb41891e8583329c23
alumnos <alumnos@409-09.local>
2019-01-09 11:15:05 +0100

    Initial Commit

---
+++ SitiosVisitados/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SitiosVisitados
+//
+//  Created by alumnos on 9/1/19.
+//  Copyright © 2019 alumnos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
